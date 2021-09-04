126fa30ae48cade17cb2634fd0a13b538d27992f
Sivert Randen <sivert.randen@hotmail.com>
2021-09-04 22:38:19 +0200

    Fix storybook scss module config ✨

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,15 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.tsx",
-    "../src/**/*.stories.mdx"
+  stories: ['../src/**/*.stories.tsx', '../src/**/*.stories.mdx'],
+  addons: [
+    {
+      name: '@storybook/preset-scss',
+      options: {
+        cssLoaderOptions: {
+          modules: true,
+        },
+      },
+    },
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+};
